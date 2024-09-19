da5840e901780e57d9b1b542f018db0f931ab915
Farhan <hey.farhan03@gmail.com>
2024-09-19 15:49:40 +0530

    Added Client Build

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(express.static(path.resolve(path.dirname(__dirname) + '/public')));
+app.use(express.static(path.resolve(path.dirname(__dirname) + '/Kaamback-frontend/client/dist')));
 
 app.get('/', (req, res) => {
 
