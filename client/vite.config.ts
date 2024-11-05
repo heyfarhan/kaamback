666dba0a4059cf519c91e3a096c6118c7b20482e
ctoKaamback <ctokaamback@gmail.com>
2024-11-05 19:18:12 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        //target: "https://geode-thinkable-stealer.glitch.me",
+        target: "https://kaamback-rose.vercel.app",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
