8c04205b1d92dac67591014f19b9e32b3892aea2
heyfarhan <113258806+heyfarhan@users.noreply.github.com>
2024-11-07 22:33:38 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const path = require('path')
+const cors = require('cors')
 
 const signupRoutes = require("./routes/signup.routes")
 const loginRoutes = require("./routes/login.routes")
